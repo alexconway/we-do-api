22a2ef8fd547071acd6bbc9e82317f3456300ef1
Alex Conway <alex@we.do>
2025-05-21 16:19:54 +0800

    fix feature update route to correct Part Studio

--- api.js
+++ api.js
@@ -22,14 +22,15 @@ app.use(express.json());
 const ON_API_KEY = process.env.ONSHAPE_API_KEY;
 const ON_SECRET = process.env.ONSHAPE_SECRET_KEY;
 
+// Constants specific to your setup
 const documentId = "fb175ab4a74c1755a9ef3489";
 const workspaceId = "70c878d3af18a26b0749b45d";
-const featureStudioElementId = "555fe81a7df93fe2e11a084d";
+const partStudioElementId = "f5c1eee7f5f2083f147d752d";
 
 app.post("/update-feature", async (req, res) => {
   const { height, width, depth } = req.body;
 
-  const url = `https://cad.onshape.com/api/partstudios/d/${documentId}/w/${workspaceId}/features`;
+  const url = `https://cad.onshape.com/api/partstudios/d/${documentId}/w/${workspaceId}/e/${partStudioElementId}/features`;
 
   try {
     const response = await axios.post(url, {
@@ -63,7 +64,6 @@ app.post("/update-feature", async (req, res) => {
     });
   }
 });
-
 
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`);
